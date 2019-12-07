83bc4501dceab06de7d87022c690d69eb02123b4
gkmishragaurav <49789867+gkmishragaurav@users.noreply.github.com>
2019-12-08 00:24:40 +0530

    new commit.

--- Problem solving/graph/graph_using_matrix.py
+++ Problem solving/graph/graph_using_matrix.py
@@ -23,6 +23,15 @@ class Graph:
                 return i
         return -1
 
+    def get_connected(self, id):
+        '''Return all non zero index'''
+        nbr=[]
+        for index in range(len(self.matrix[id-1])):
+            if self.matrix[index]:
+                nbr.append(index+1)
+
+        return nbr
+
     def add_edge(self, frm, to):
         self.matrix[frm-1][to-1] = 1
 
@@ -38,13 +47,46 @@ class Graph:
     def __str__(self):
         return str(self.matrix)
 
+    def dfs(self, start):
+        stack=[start]
+        visited=[]
+        while stack:
+            temp=stack.pop() # one difference from BFS is to pop last element here instead of first one
+            if temp not in visited:
+                visited.append(temp)
+
+                for nbr in self.get_connected(temp):
+                    if nbr not in visited:
+                        stack.append(nbr)
+
+        return visited
+
+    def bfs(self, start):
+        stack=[start]
+        visited=[]
+        while stack:
+            temp=stack.pop(0)
+            if temp not in visited:
+                visited.append(temp)
+
+                for nbr in self.get_connected(temp):
+                    if nbr not in visited:
+                        stack.append(nbr)
+
+        return visited
+
 
 g=Graph(5)
 g.add_edge(1, 4)
+g.add_edge(1, 3)
+g.add_edge(1, 5)
 g.add_edge(4, 2)
 g.add_edge(4, 5)
 g.add_edge(2, 5)
 g.add_edge(5, 3)
 
 print str(g)
+print g.get_connected(1)
+print "dfs", g.dfs(1)
+print "bfs",g.bfs(1)
 
