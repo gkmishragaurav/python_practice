663d2c8feaab12cd9e2b8198d0fa6e1a70d68ee6
gkmishragaurav <49789867+gkmishragaurav@users.noreply.github.com>
2020-02-14 22:04:25 +0530

    new commit.

--- Problem solving/Hashing/create_hash.py
+++ Problem solving/Hashing/create_hash.py
@@ -1,19 +1,75 @@
 # Implementation of separate chaining collision hashtable of given size n.
 # Allocate an array of size n
 # Search/insert/delete option should be implemented.
+# features:
+# resize the hash when bucket is full.
 
-class HastTable:
-    def __init__(self):
-        pass
+class Employee():
+    bucket_size = 4
+    total_buckets = 5
+    size=0
+    def __init__(self, capacity=bucket_size*total_buckets):
+        self.capacity = capacity
+        self.bucket=[[]]*self.total_buckets
 
-    def put(self, key, value):
-        '''This function is to push value in hash table'''
-        pass
+    def hash_Function(self, key):
+        return key%self.total_buckets
 
-    def get(self, key):
-        '''This function is to get a value.'''
-        pass
+    def resize(self):
+        self.bucket_size+=1
+        self.capacity = self.bucket_size*self.total_buckets
+        print(self.bucket_size, self.capacity)
 
-    def delete(self, key):
-        '''For deleting a key in hash table'''
-        pass
+    def set(self, key, value):
+        hash_key = self.hash_Function(key)
+        bucket = self.bucket[hash_key]
+        if not bucket:
+            self.bucket[hash_key] = [(key, value)]
+        else:
+            found = self.search_in_bucket(bucket, key)
+            if found[1]:
+                if found[1] != value:
+                    self.bucket[hash_key].remove((key, value))
+                else:
+                    return
+            else:
+                self.bucket[hash_key].append((key, value))
+                self.size+=1
+                if len(self.bucket[hash_key]) == self.bucket_size:
+                    self.resize()
+        return 0
+
+    def search_in_bucket(self, bucket, key):
+        found=False
+        for n, item in enumerate(bucket):
+            if item[0] == key:
+                return item
+        return found, None
+
+    def get(self, item):
+        hash_key = self.hash_Function(item)
+        bucket = self.bucket[hash_key]
+        if bucket:
+            found = self.search_in_bucket(bucket, item)
+            if found[0]:
+                return found[1]
+        return False
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def __getitem__(self, item):
+        return self.get(item)
+
+    def __str__(self):
+        return str(self.bucket)
+
+emp = Employee()
+emp[12] = 'fwgwe'
+emp[2] = "rgrg"
+emp[22] = 'rgrger'
+emp[32] = "rgrg"
+emp[42] = "rgrg"
+emp[52] = "rgrg"
+print(emp[22])
+print(emp.size, emp.capacity, emp)
--- test.py
+++ test.py
@@ -1,2 +1,57 @@
-import node.Node
+class Employee():
+    bucket_size = 4
+    total_buckets = 5
+    size=0
+    def __init__(self, capacity=bucket_size*total_buckets):
+        self.capacity = capacity
+        self.bucket=[[]]*self.total_buckets
 
+    def hash_Function(self, key):
+        return key%5
+
+    def set(self, key, value):
+        hash_key = self.hash_Function(key)
+        bucket = self.bucket[hash_key]
+        if not bucket:
+            self.bucket[hash_key] = [(key, value)]
+        else:
+            found = self.search_in_bucket(bucket, key)
+            if found[1]:
+                if found[1] != value:
+                    self.bucket[hash_key].remove((key, value))
+                else:
+                    return
+            self.bucket[hash_key].append((key, value))
+        return 0
+
+    def search_in_bucket(self, bucket, key):
+        found=False
+        for n, item in enumerate(bucket):
+            if item[0] == key:
+                return item
+        return found, None
+
+    def get(self, item):
+        hash_key = self.hash_Function(item)
+        bucket = self.bucket[hash_key]
+        if bucket:
+            found = self.search_in_bucket(bucket, item)
+            if found[0]:
+                return found[1]
+        return False
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def __getitem__(self, item):
+        return self.get(item)
+
+    def __str__(self):
+        return str(self.bucket)
+
+emp = Employee()
+emp[12] = 'fwgwe'
+emp[13] = "rgrg"
+emp[22] = 'rgrger'
+print(emp[22])
+print(emp)
