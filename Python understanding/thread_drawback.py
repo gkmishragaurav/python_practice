4dea7d763671a641c8c8b3c479d47983bfbb6975
gkmishragaurav <49789867+gkmishragaurav@users.noreply.github.com>
2020-11-06 14:26:45 +0530

    New notes.

--- Python understanding/thread_drawback.py
+++ Python understanding/thread_drawback.py
@@ -2,7 +2,7 @@
 import time
 from threading import Thread
 
-COUNT = 60000000
+COUNT = 600000000
 
 def countdown(n):
     while n>0:
@@ -20,17 +20,20 @@ def countdown(n):
     while n>0:
         n -= 1
 
-t1 = Thread(target=countdown, args=(COUNT//3,))
-t2 = Thread(target=countdown, args=(COUNT//3,))
-t3 = Thread(target=countdown, args=(COUNT//3,))
+t1 = Thread(target=countdown, args=(COUNT//4,))
+t2 = Thread(target=countdown, args=(COUNT//4,))
+t3 = Thread(target=countdown, args=(COUNT//4,))
+t4 = Thread(target=countdown, args=(COUNT//4,))
 
 start = time.time()
 t1.start()
 t2.start()
 t3.start()
+t4.start()
 t1.join()
 t2.join()
 t3.join()
+t4.join()
 end = time.time()
 
 print('Time taken in seconds -', end - start)
