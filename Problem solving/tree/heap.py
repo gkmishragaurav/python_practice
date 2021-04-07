c60521df3dc371b1d16fe97fea1b50481b59cbda
gkmishragaurav <49789867+gkmishragaurav@users.noreply.github.com>
2021-04-07 10:07:11 +0530

    Update heap.py

--- Problem solving/tree/heap.py
+++ Problem solving/tree/heap.py
@@ -1,54 +1,46 @@
-import pdb
-def parent(a, i):
-    return (i-1)/2
+def left(i):
+    return 2 * i + 1
 
-def left(a, i):
-    return(2*i+1)
 
-def right(a, i):
-    return (2*i+2)
+def right(i):
+    return 2 * i + 2
 
-def swap(a,b):
-    return b,a
 
-def min_heapify(a, end=None):
-    for i in range(end/2):
-        left_child = left(a, i)
-        right_child = right(a, i)
-        if a[left_child] < a[i] and left_child < end:
-            if a[right_child] < a[left_child]:
-                a[i], a[right_child] = a[right_child], a[i]
-            else:
-                a[i], a[left_child] = a[left_child], a[i]
-        if a[right_child] < a[i] and right_child < end:
-            a[i], a[right_child] = a[right_child], a[i]
+def max_heapfy(a, start, end, limit):
+    i = end
+    while i >= start:
+        l = left(i)
+        r = right(i)
+        l = a[l] if l <= limit else None
+        r = a[r] if r <= limit else None
+        max_tuple = [a[i]]
+        if l: max_tuple.append(l)
+        if r: max_tuple.append(r)
+        if max(max_tuple) != a[i]:
+            if l and r:
+                if r > l:
+                    a[i], a[right(i)] = a[right(i)], a[i]
+
+                else:
+                    a[i], a[left(i)] = a[left(i)], a[i]
+            elif l:
+                a[i], a[left(i)] = a[left(i)], a[i]
+
+            elif r:
+                a[i], a[right(i)] = a[right(i)], a[i]
+
+        i = i - 1
+
     return a
 
-def max_heapify(a, end=None):
-    for i in range(end/2):
-        left_child = left(a, i)
-        right_child = right(a, i)
-        if a[left_child] > a[i] and left_child < end:
-            if a[right_child] > a[left_child]:
-                a[i], a[right_child] = a[right_child], a[i]
-            else:
-                a[i], a[left_child] = a[left_child], a[i]
-        if a[right_child] > a[i] and right_child < end:
-            a[i], a[right_child] = a[right_child], a[i]
-    return a
+a = [3, 8, 2, 5, 6, 1, 9]
 
 def heap_sort(a):
     last = len(a)
-    while(last):
-        a = min_heapify(a, last)
+    while last:
+        max_heapfy(a, 0, last-1, last-1)
         a[0], a[last-1] = a[last-1], a[0]
-        last = last - 1
-    return a
+        last=last-1
 
-a = [3, 9, 2, 5, 6, 1, 8]
-print a
-print heap_sort(a)
-
-
-
-
+heap_sort(a)
+print(a)
