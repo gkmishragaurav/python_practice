48c7d927f50d0bc06c7f871d536e8128ac18d82b
gkmishragaurav <49789867+gkmishragaurav@users.noreply.github.com>
2021-05-12 13:30:28 +0530

    Update find_sum_all_sub_set.py

--- Problem solving/find_sum_all_sub_set.py
+++ Problem solving/find_sum_all_sub_set.py
@@ -30,6 +30,26 @@ def find_sum_all_sub_set(a, sum, curr_sum, st):
         curr_sum = curr_sum-a[i]
         st = copy.deepcopy(st1)
 
+def find_sum_all_sub_set2(a, s):
+    # This is much faster solution than earlier solution.
+    def util(cur, pos, target):
+        if target == 0:
+            arr.append(cur[:])
+            return
+
+        for i in range(pos, l):
+            if a[i] > target:
+                break
+            cur.append(a[i])
+            util(cur, i, target-a[i])
+            cur.pop()
+
+    l = len(a)
+    a.sort()
+    arr=[]
+    util([], 0, s)
+    return arr
+
 a = [3, 1, 4, 12, 5, 2, 7, 6, 8]
 sum = 9
 find_sum_all_sub_set(a, sum, 0, [])
