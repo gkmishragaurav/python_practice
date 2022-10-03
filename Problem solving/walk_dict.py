54d0d1d6c694f245bbb172d4017578da6b686137
gkmishragaurav <49789867+gkmishragaurav@users.noreply.github.com>
2022-10-03 16:08:59 +0530

    Update walk_dict.py

--- Problem solving/walk_dict.py
+++ Problem solving/walk_dict.py
@@ -28,13 +28,51 @@ l = [{
                 },
             },
         },
-    }]
+    },
+  {
+    "items":
+      {
+        "item":
+          [
+            {
+              "id": "0001",
+              "type": "donut",
+              "name": "Cake",
+              "ppu": 0.55,
+              "batters":
+                {
+                  "batter":
+                    [
+                      {"id": "1001", "type": "Regular"},
+                      {"id": "1002", "type": "Chocolate"},
+                      {"id": "1003", "type": "Blueberry"},
+                      {"id": "1004", "type": "Devil's Food"}
+                    ]
+                },
+              "topping":
+                [
+                  {"id": "5001", "type": "None"},
+                  {"id": "5002", "type": "Glazed"},
+                  {"id": "5005", "type": "Sugar"},
+                  {"id": "5007", "type": "Powdered Sugar"},
+                  {"id": "5006", "type": "Chocolate with Sprinkles"},
+                  {"id": "5003", "type": "Chocolate"},
+                  {"id": "5004", "type": "Maple"}
+                ]
+            }
+          ]
+      }
+  }
+]
 
 def walk_dict(d, k, v):
   for key,value in d.iteritems():
     if isinstance(value, dict):
       k.append(key)
       walk_dict(d[key], k, v)
+    elif isinstance(value, list):
+      k.append(key)
+      walk_list(value, k, v)
     else:
       k.append(key)
       v.append(value)
