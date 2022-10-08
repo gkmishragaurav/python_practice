443bec1b12852b3ca0300cb81d6d5452b0874bf6
gkmishragaurav <49789867+gkmishragaurav@users.noreply.github.com>
2022-10-08 12:52:10 +0530

    Update depth_of_dict.py

--- Python understanding/depth_of_dict.py
+++ Python understanding/depth_of_dict.py
@@ -1,3 +1,7 @@
+# Write a code to find the depth of a dict.
+# Depth of the dict - max depth of a dict
+# depth - I a nested dict, going from one key to next nexted key till there is no more key will be described as depth of key.
+
 def depth(d, level=1):
     if not isinstance(d, dict) or not d:
         return level
